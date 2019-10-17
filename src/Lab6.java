2f31be3b4335ba3b1d0d5452e6bc7c64d8b9c783
robmalec <robmalec@umich.edu>
2019-10-17 15:40:49 -0400

    initial commit

---
+++ src/Lab6.java
@@ -0,0 +1,42 @@
+package co.grandcircus;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lab6 {
+	public static void main(String[] args) {
+		char again = 'y';
+		Scanner scn = new Scanner(System.in);
+		int numSides = 0;
+		do {
+			try {
+				System.out.println("How many sides should each die have?");
+				numSides = scn.nextInt();
+				
+				System.out.println(getDieRoll(numSides));
+				System.out.println(getDieRoll(numSides));
+				
+				System.out.println("Roll again?");
+				again = scn.next().charAt(0);
+				if ((again != 'y') && (again != 'n')) {
+					again = 'y';
+					throw new Exception();
+				}
+			}
+			catch (Exception e){
+				System.out.println("Invalid input.  Try again.");
+				scn.nextLine();
+			}
+		} while (again == 'y');
+	}
+	public static int getDieRoll(int numSides) {
+		return ((int) Math.ceil(Math.random() * numSides));
+	}
+	public static int getDieRoll2(int numSides) {
+		Random r = new Random();
+		
+		return r.nextInt(numSides);
+	}
+}
+
+
